25d8f2a57cba92931935c55ea435f2415ef0a56a
Ashley McCloskey <43911764+ashleymcc@users.noreply.github.com>
2019-05-01 20:54:22 -0700

    Fixing Path

---
+++ app/data/friends.js
@@ -0,0 +1,46 @@
+var friends = [
+    
+    {
+      name: "Anna Kendrick",
+      photo:
+        "https://m.media-amazon.com/images/M/MV5BMjIzOTA0OTQyN15BMl5BanBnXkFtZTcwMjE1OTIwMw@@._V1_UY317_CR5,0,214,317_AL_.jpg",
+      scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
+    },
+    {
+      name: "Bella Thorne",
+      photo:
+        "https://m.media-amazon.com/images/M/MV5BMjIwNzY5MTI2Ml5BMl5BanBnXkFtZTgwNjE1NjY0ODE@._V1_UY317_CR17,0,214,317_AL_.jpg",
+      scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+    },
+    {
+        name: "Vin Diesel",
+        photo:
+          "https://m.media-amazon.com/images/M/MV5BMjExNzA4MDYxN15BMl5BanBnXkFtZTcwOTI1MDAxOQ@@._V1_UY317_CR7,0,214,317_AL_.jpg",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+      },
+      {
+        name: "Tyrone Magnus",
+        photo:
+          "https://m.media-amazon.com/images/M/MV5BMzcxODk5MzE2NV5BMl5BanBnXkFtZTgwMTE5MTY4OTE@._V1_UY317_CR51,0,214,317_AL_.jpg",
+        scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+      },
+      {
+        name: "Paul Rudd",
+        photo:
+          "https://m.media-amazon.com/images/M/MV5BMTY4NTEwNDg1MV5BMl5BanBnXkFtZTgwODMwMDA0ODE@._V1_UY317_CR20,0,214,317_AL_.jpg",
+        scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+      },
+      {
+        name: "Ashley Benson",
+        photo:
+          "https://m.media-amazon.com/images/M/MV5BMTkzMDY1NDk5MV5BMl5BanBnXkFtZTgwNTMxNzIyOTE@._V1_UY317_CR51,0,214,317_AL_.jpg",
+        scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+      },
+      {
+        name: "Chad Michael Murrayi",
+        photo:
+          "https://m.media-amazon.com/images/M/MV5BM2M1NzEyNjItYWVhYS00ZGJkLTgxMDctY2RlZGE0Yjg0ZWIyXkEyXkFqcGdeQXVyMjQ0NDAxNDc@._V1_UY317_CR20,0,214,317_AL_.jpg",
+        scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+      }
+  ];
+  module.exports = friends;
